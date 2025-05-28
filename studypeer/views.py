12ff33fd3faf98c00ce4d9bc5736f208c665f628
dossdortival <dossunit@gmail.com>
2025-05-28 22:04:08 +0300

    implement authentication

--- studypeer/views.py
+++ studypeer/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db import IntegrityError
 
-# Create your views here.
+from .models import User
+
+
+def index(request):
+    return render(request, "studypeer/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication was successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "studypeer/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "studypeer/login.html")
+    
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+
+        if password != confirmation:
+            return render(request, "studypeer/register.html", {
+                "message": "Passwords must match."
+            })
+
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+             
+        except IntegrityError:
+            return render(request, "studypeer/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "studypeer/register.html")
