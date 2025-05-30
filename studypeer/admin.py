05c6b954bd05933985f95b426ab185f011936fb8
dossdortival <dossunit@gmail.com>
2025-05-30 17:43:48 +0300

    update core models & admin panel

--- studypeer/admin.py
+++ studypeer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Subject, StudyGroup, Membership
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(StudyGroup)
+admin.site.register(Membership)
--- studypeer/models.py
+++ studypeer/models.py
@@ -5,21 +5,21 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
     bio = models.TextField(_('bio'), blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
-    university = models.CharField(_('university'), max_length=100, blank=True)
+    interests = models.CharField(_('interests'), max_length=255, blank=True)
+    location = models.CharField(_('location'), max_length=100, blank=True)
     
     def __str__(self):
         return self.username
 
 class Subject(models.Model):
-    name = models.CharField(_('name'), max_length=100)
-    code = models.CharField(_('code'), max_length=20, blank=True)
+    name = models.CharField(_('name'), max_length=100, unique=True)
     description = models.TextField(_('description'), blank=True)
     
     def __str__(self):
-        return f"{self.name} ({self.code})" if self.code else self.name
+        return self.name
 
 class StudyGroup(models.Model):
-    name = models.CharField(_('name'), max_length=100)
+    title = models.CharField(_('title'), max_length=100, unique=True, default='Untitled Group')
     description = models.TextField(_('description'))
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='groups')
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_groups')
@@ -31,7 +31,21 @@ class StudyGroup(models.Model):
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
     
     def __str__(self):
-        return f"{self.name} - {self.subject.name}"
+        return self.title
     
-    def available_slots(self):
-        return self.max_members - self.members.count()
+    def current_member_count(self):
+        return self.memberships.count()
+    
+    def is_full(self):
+        return self.current_member_count() >= self.max_members
+    
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memberships')
+    group = models.ForeignKey(StudyGroup, on_delete=models.CASCADE, related_name='memberships')
+    joined_at = models.DateTimeField(_('joined at'), auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'group')
+    
+    def __str__(self):
+        return f"{self.user.username} in {self.group.title}"
